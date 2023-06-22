d5ce634764c7ab69a218b096bfcdfdf68ae40cf8
nick thomas <nichoth@gmail.com>
2023-06-21 17:08:17 -0700

    fix lint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
-export function example ():void {
-    console.log('hello')
+import { Implementation } from '@oddjs/odd/components/crypto/implementation'
+import { SignedRequest, create as createMsg } from '@ssc-hermes/message'
+import { KyInstance } from 'ky/distribution/types/ky'
+
+// need to pass in starting sequence number
+export function AuthRequest (
+    ky:KyInstance,
+    crypto:Implementation,
+    startingSeq:number|Storage
+):KyInstance {
+    return ky.create({
+        hooks: {
+            beforeRequest: [
+                async req => {
+                    const seq = typeof startingSeq === 'number' ?
+                        startingSeq :
+                        1
+                    req.headers.set('Authorization',
+                        await createHeader(crypto, seq))
+                }
+            ]
+        }
+    })
+}
+
+export async function createHeader (crypto:Implementation, seq:number)
+:Promise<string> {
+    const newHeader = JSON.stringify(await createMsg(crypto, { seq }))
+    return `Bearer ${newHeader}`
+}
+
+/**
+ * Take the header returned from `createHeader`
+ */
+export function parseHeader (header:string):SignedRequest<{ seq:number }> {
+    const json = header.split(' ')[1]
+    return JSON.parse(json)
 }
--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,61 @@
 import { test } from '@socketsupply/tapzero'
+import { assemble } from '@oddjs/odd'
+import { components } from '@ssc-hermes/node-components'
+import { AuthRequest, createHeader, parseHeader } from '../dist/index.js'
+import ky from 'ky-universal'
 
-test('example', async t => {
-    t.ok('ok', 'should be an example')
+let crypto
+test('setup', async t => {
+    const program = await assemble({
+        namespace: { creator: 'test', name: 'testing' },
+        debug: false
+    }, components)
+    crypto = program.components.crypto
+
+    t.ok(program, 'create a program')
+})
+
+let header:string
+test('create a header', async t => {
+    header = await createHeader(crypto, 1)
+    t.ok(header, 'should return a header')
+    t.ok(header.includes('Bearer'), 'should be a Bearer token')
+})
+
+test('parse header', t => {
+    const obj = parseHeader(header)
+    t.equal(obj.seq, 1, 'should have the right sequence number')
+})
+
+let req
+test('create instance', async t => {
+    req = AuthRequest(ky, crypto, 0)
+
+    await req.get('https://example.com/', {
+        hooks: {
+            afterResponse: [
+                (request:Request) => {
+                    const obj = parseHeader(
+                        request.headers.get('Authorization') as string
+                    )
+                    t.equal(obj.seq, 1, 'should have the right sequence')
+                }
+            ]
+        }
+    })
+})
+
+test('make another request', async t => {
+    await req.get('https://example.com', {
+        hooks: {
+            afterResponse: [
+                (request:Request) => {
+                    const obj = parseHeader(
+                        request.headers.get('Authorization') as string
+                    )
+                    t.equal(obj.seq, 2, 'should increment the sequence number')
+                }
+            ]
+        }
+    })
 })
