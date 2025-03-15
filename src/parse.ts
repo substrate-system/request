9a9389f06f1488ee7eb7f049745ae8830810e291
nichoth <nichoth@nichoth.com>
2025-03-14 17:39:27 -0700

    better types

--- src/parse.ts
+++ src/parse.ts
@@ -4,7 +4,7 @@ import type { Token } from './index.js'
  * Take the header returned from `createHeader`
  * @returns A parsed JSON value (an object)
  */
-export function parseHeader<T> (header:string):Token<T> {
+export function parseHeader<T=Token<{ seq:number }>> (header:string):Token<T> {
     const json = atob(header.split(' ')[1])
     const obj = JSON.parse(json)
     if (typeof obj.seq === 'string') obj.seq = parseInt(obj.seq)
